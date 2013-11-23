bd1cdb6c8a28eb3249219e6fee0cdf5c4ea2816d
ahocevar <andreas.hocevar@gmail.com>
2013-11-23 13:12:05 +0100

    Adding gmaps-v3 version of the example

---
+++ gmaps-v3.js
@@ -0,0 +1,38 @@
+var exampleLoc = new google.maps.LatLng(-25.363882,131.044922);
+
+var map = new google.maps.Map(document.getElementById('map'),{
+  zoom: 4,
+  center: exampleLoc,
+  disableDefaultUI: true,
+  zoomControl: true,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var infowindow = new google.maps.InfoWindow({
+    content: 'Hello, world!'
+});
+
+var marker = new google.maps.Marker({
+    position: exampleLoc,
+    map: map
+});
+
+google.maps.event.addListener(marker, 'click', function() {
+  infowindow.open(map,marker);
+});
+
+var circle = new google.maps.Circle({
+  strokeColor: '#FF0000',
+  strokeOpacity: 0.8,
+  strokeWeight: 3,
+  fillColor: '#FF0000',
+  fillOpacity: 0.1,
+  map: map,
+  center: exampleLoc,
+  radius: 400000
+});
+
+var exampleKml = new google.maps.KmlLayer({
+  url: 'data/example.kml',
+  clickable: false
+});
