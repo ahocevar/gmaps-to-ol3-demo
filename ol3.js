c6e4fce26493cfd67999f376245c3e13ce88afc1
ahocevar <andreas.hocevar@gmail.com>
2013-11-24 22:34:23 +0100

    Adding ol3 version of example

---
+++ ol3.js
@@ -0,0 +1,37 @@
+var exampleLoc = ol.proj.transform(
+    [131.044922, -25.363882], 'EPSG:4326', 'EPSG:3857');
+
+var map = new ol.Map({
+  target: 'map',
+  renderer: ol.RendererHint.CANVAS,
+  view: new ol.View2D({
+    zoom: 4,
+    center: exampleLoc
+  }),
+  layers: [new ol.layer.Tile({source: new ol.source.MapQuestOSM()})]
+});
+
+map.addOverlay(new ol.Overlay({
+  position: exampleLoc,
+  element: $('<div>').addClass('marker')
+      .tooltip({title: 'Hello, world!', trigger: 'click'})
+}));
+
+map.addOverlay(new ol.Overlay({
+  element: $('<div>').addClass('circle'),
+  position: exampleLoc
+}));
+function setCircleSize() {
+  var size = 2 * 400000 / map.getView().getResolution() + 'px';
+  $('.circle').width(size).height(size);
+}
+setCircleSize();
+map.getView().on('change:resolution', setCircleSize);
+
+var exampleKml = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'data/example.kml',
+    parser: new ol.parser.KML()
+  })
+});
+map.addLayer(exampleKml);
