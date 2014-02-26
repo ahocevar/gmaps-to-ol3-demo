2eca15948f30d615c36aa7164e92d0dd4acef550
ahocevar <andreas.hocevar@gmail.com>
2014-02-26 23:18:22 +0100

    Updating to latest ol3 master and Bootstrap 3.1.1

--- ol3.js
+++ ol3.js
@@ -3,35 +3,36 @@ var exampleLoc = ol.proj.transform(
 
 var map = new ol.Map({
   target: 'map',
-  renderer: ol.RendererHint.CANVAS,
+  renderer: 'canvas',
   view: new ol.View2D({
+    projection: 'EPSG:3857',
     zoom: 3,
     center: exampleLoc
   }),
-  layers: [new ol.layer.Tile({source: new ol.source.MapQuestOSM()})]
+  layers: [
+    new ol.layer.Tile({source: new ol.source.MapQuest({layer: 'osm'})})
+  ]
 });
 
 map.addOverlay(new ol.Overlay({
   position: exampleLoc,
-  element: $('<div>').addClass('marker')
+  element: $('<img src="resources/img/marker-blue.png">')
+      .css({marginTop: '-200%', marginLeft: '-50%', cursor: 'pointer'})
       .tooltip({title: 'Hello, world!', trigger: 'click'})
 }));
 
-map.addOverlay(new ol.Overlay({
-  element: $('<div>').addClass('circle'),
-  position: exampleLoc
-}));
-function setCircleSize() {
-  var size = 2 * 400000 / map.getView().getResolution() + 'px';
-  $('.circle').width(size).height(size);
-}
-setCircleSize();
-map.getView().on('change:resolution', setCircleSize);
+map.on('postcompose', function(evt) {
+  evt.vectorContext.setFillStrokeStyle(
+      new ol.style.Fill({color: 'rgba(255, 0, 0, .1)'}),
+      new ol.style.Stroke({color: 'rgba(255, 0, 0, .8)', width: 3}));
+  evt.vectorContext.drawCircleGeometry(
+      new ol.geom.Circle(exampleLoc, 400000));
+});
 
 var exampleKml = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    url: 'data/example.kml',
-    parser: new ol.parser.KML()
+  source: new ol.source.KML({
+    projection: 'EPSG:3857',
+    url: 'data/example.kml'
   })
 });
 map.addLayer(exampleKml);
